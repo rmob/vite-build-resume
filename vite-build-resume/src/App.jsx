6e90039d9d4364366bd459b926323caf45dad18b
rmob <riley.obryan@gmail.com>
2023-04-15 10:07:10 -0700

    added rest of links and pictures that I could so far to portfolio page

--- vite-build-resume/src/App.jsx
+++ vite-build-resume/src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-// import './App.css'
-import './index.css'
+
 
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
